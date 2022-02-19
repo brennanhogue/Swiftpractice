c2f06d10e34361dca85e414a3800a03391698769
Brennan Hogue <hogue.brennan@gmail.com>
2022-02-19 10:28:23 -0700

    Initial Commit

---
+++ Swiftpractice/Day 1.swift
@@ -0,0 +1,29 @@
+//
+//  Day 1.swift
+//  Swiftpractice
+//
+//  Created by Brennan Hogue on 2/19/22.
+//
+
+import Foundation
+
+
+var age = 38
+var population = 8_000_000
+var firstName = "Brennan"
+var lastName = "Hogue"
+
+var pi = 3.141
+var awesome = true
+
+var score = 69
+var str = "Your score was \(score)"
+var results = "The test result are here: \(str)"
+var name = "\(firstName) \(lastName)"
+
+let brennan = "hogue"
+
+var percentage: Double = 99
+
+
+
