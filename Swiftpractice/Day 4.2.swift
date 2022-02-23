0f92e72659245c4ccf6eaf64652e342035943d24
Brennan Hogue <hogue.brennan@gmail.com>
2022-02-22 23:14:29 -0700

    Exiting loops

---
+++ Swiftpractice/Day 4.2.swift
@@ -0,0 +1,22 @@
+//
+//  Day 4.2.swift
+//  Swiftpractice
+//
+//  Created by Brennan Hogue on 2/22/22.
+//
+
+import Foundation
+
+var countDown = 10
+
+while countDown >= 0 {
+    print(countDown)
+    countDown -= 1
+    
+    if countDown == 4 {
+        print("I'm bored, let's go now!")
+        break
+    }
+
+
+}
